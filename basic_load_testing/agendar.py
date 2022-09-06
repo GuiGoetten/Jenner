cc6d1f9e85507e266bbc7c144011701d4d00a4de
CallebCecco <calleb.malinoski@gmail.com>
2022-09-05 22:45:08 -0300

    Adiciona csv com dados para teste

--- basic_load_testing/agendar.py
+++ basic_load_testing/agendar.py
@@ -1,16 +1,20 @@
 import time
 from locust import HttpUser, task, between
 
+from locust_plugins.csvreader import CSVReader
+
+ssn_reader = CSVReader("dados.csv")
 
 class WebsiteUser(HttpUser):
     wait_time = between(1, 5)
 
     @task
     def on_agendar(self):
+      customer = next(ssn_reader)
        self.client.post("/api", json={
-  "cpf": "07619933980",
-  "nomePessoa": "Teste",
-  "dataNascimento": "2022-06-15",
+      "cpf": customer[0],
+        "nomePessoa": customer[1],
+        "dataNascimento": customer[2],
   "nomeVacina": "Aztrazeneca",
   "dose": 1,
   "dataAgendamento": "2022-06-15"
--- basic_load_testing/aplicar.py
+++ basic_load_testing/aplicar.py
@@ -1,27 +1,21 @@
 import time
 from locust import HttpUser, task, between
+from locust_plugins.csvreader import CSVReader
 
+ssn_reader = CSVReader("dados.csv")
 
 class WebsiteUser(HttpUser):
     wait_time = between(1, 5)
 	
     @task
     def on_start(self):
+        customer = next(ssn_reader)
         self.client.post("/api", json={
-    "cpf": "07619933980",
-    "nomePessoa": "Lucas",
-    "dataNascimento": "1993-07-25T00:37:22.841Z",
-    "nomeVacina": "Pfizer",
-    "dose": 1,
-    "dataAgendamento": "2022-06-15",
-    "dataAplicada": "2022-06-15"
-    })
-        self.client.post("/api", json={
-    "cpf": "06086088900",
-    "nomePessoa": "Guilherme",
-    "dataNascimento": "1991-03-12T00:37:22.841Z",
-    "nomeVacina": "Jensen",
-    "dose": 1,
-    "dataAgendamento": "2022-06-15",
-    "dataAplicada": "2022-06-15"
-    })
+        "cpf": customer[0],
+        "nomePessoa": customer[1],
+        "dataNascimento": customer[2],
+        "nomeVacina": "Pfizer",
+        "dose": 1,
+        "dataAgendamento": "2022-06-15",
+        "dataAplicada": "2022-06-15"
+        })
