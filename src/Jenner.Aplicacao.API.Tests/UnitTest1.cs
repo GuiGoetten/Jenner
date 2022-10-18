b4c1c552fc9daa5b52a259d8e9893f1f6c2b864c
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-10-17 21:57:03 -0300

    tests: Create project for API tests

---
+++ src/Jenner.Aplicacao.API.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Jenner.Aplicacao.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true, "this is supposed to work!");
+        }
+    }
+}
