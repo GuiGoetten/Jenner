17efdcfd487dfe7f3d386226809ce1f476b62197
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-11-07 21:34:40 -0300

    housekeeping: Reduce smells on AgendadorWorker

--- src/Jenner.Carteira.Agendador.Worker/Services/Consumer/AgendadorWorker.cs
+++ src/Jenner.Carteira.Agendador.Worker/Services/Consumer/AgendadorWorker.cs
@@ -3,23 +3,23 @@ using CloudNative.CloudEvents.SystemTextJson;
 using Confluent.Kafka;
 using Jenner.Carteira.Agendador.Worker.Providers;
 using Jenner.Comum;
-using Jenner.Comum.Models;
 using MediatR;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Jenner.Carteira.Agendador.Worker.Services.Consumer
 {
     public class AgendadorWorker : KafkaConsumerBase
     {
+        private readonly ISender _sender;
+        private readonly ILogger<AgendadorWorker> _logger;
 
-        public ISender sender;
-        public AgendadorWorker(IServiceProvider serviceProvider,  ISender sender) : base(serviceProvider, new JsonEventFormatter<Comum.Models.Carteira>())
+        public AgendadorWorker(IServiceProvider serviceProvider,  ISender sender, ILogger<AgendadorWorker> logger) : base(serviceProvider, new JsonEventFormatter<Comum.Models.Carteira>())
         {
-            this.sender = sender ?? throw new ArgumentNullException(nameof(sender));
+            _sender = sender ?? throw new ArgumentNullException(nameof(sender));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         protected override async Task DoScopedAsync(CancellationToken cancellationToken)
@@ -29,17 +29,18 @@ namespace Jenner.Carteira.Agendador.Worker.Services.Consumer
                 throw new ArgumentException("For some reason the Consumer is null, this shouldn't happen.");
             }
 
+            _logger.LogDebug("Subscribing to topic {kafkaTopic}", Constants.CloudEvents.AplicadaTopic);
             KafkaConsumer.Subscribe(Constants.CloudEvents.AplicadaTopic);
 
             while (!cancellationToken.IsCancellationRequested)
             {
                 try
                 {
-                    //TODO: Pra retirar depois
-                    Console.WriteLine(cancellationToken.IsCancellationRequested); 
-
+                    _logger.LogDebug("Cancellation Status: {cancelStatus}", cancellationToken.IsCancellationRequested);
+                    _logger.LogDebug("Waiting to Consume from topic...");
                     ConsumeResult<string, byte[]> result = KafkaConsumer.Consume(cancellationToken);
-
+                    
+                    _logger.LogDebug("Message received! Parsing to CloudEvent and working on it");
                     var cloudEvent = result.Message.ToCloudEvent(cloudEventFormatter);
 
                     if (cloudEvent.Data is Comum.Models.Carteira mensagem)
@@ -55,25 +56,26 @@ namespace Jenner.Carteira.Agendador.Worker.Services.Consumer
                                 UltimaAplicacao = mensagem.GetLatestAplicacao()
                             };
 
-                            await sender.Send(carteiraCreate, cancellationToken);
+                            await _sender.Send(carteiraCreate, cancellationToken);
                         }
                         catch (Exception e)
                         {
-                            Console.WriteLine($"Não recebi uma aplicacao {e.Message}");
+                            _logger.LogError(e, "Unexpected behavior while creating carteira: {errorMessage}", e.Message);
                         }
                     }
                     else
                     {
-                        Console.WriteLine("Não é uma carteira...");
+                        _logger.LogWarning("Message isn't a carteira but instead a {messageType}", cloudEvent.Type);
                     }
                 }
                 catch (ConsumeException e)
                 {
-                    Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    _logger.LogError(e, "Error while parsing message, discarding it");
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex.Message);
+                    _logger.LogError(ex, "A catastrophic error happened: {errorMessage}", ex.Message);
+                    throw;
                 }
             }
 
